dd27e0078872e3ce5c0a06497e4b9327407897ca
rafsan-jany <rafsan.mbstu@gmail.com>
2024-09-11 15:22:23 +0600

    done update operations

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { createEmployee, getEmployee } from '../services/EmployeeService'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
@@ -30,17 +30,28 @@ const EmployeeComponent = () => {
 
 
 
-    function saveEmployee(e) {
+    function saveOrUpdateEmployee(e) {
         e.preventDefault();
 
         if(validateForm()){
             const employee = {first_name, last_name, email}
             console.log(employee)
-    
-            createEmployee(employee).then((response) => {
-                console.log(response.data);
-                navigator('/employees')
-            })
+
+            if(id) {
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.log(error);
+                })
+            }else {
+                createEmployee(employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            }
         }
     }
 
@@ -134,7 +145,7 @@ const EmployeeComponent = () => {
                             </input>
                             { errors.email && <div className='invalid-feedback'>{errors.email}</div>}
                         </div>
-                        <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                        <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
                     </form>
                 </div>
             </div>
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 
-const REST_API_BASE_URL = "http://localhost:8000/users/"
+const REST_API_BASE_URL = "http://localhost:8000/users"
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
 export const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employee);
 
-export const getEmployee = (employeeId) => axios.get(REST_API_BASE_URL + employeeId);
+export const getEmployee = (employeeId) => axios.get(REST_API_BASE_URL + '/' + employeeId);
+
+export const updateEmployee = (employeeId, employee) => axios.put(REST_API_BASE_URL + '/' + employeeId, employee);
