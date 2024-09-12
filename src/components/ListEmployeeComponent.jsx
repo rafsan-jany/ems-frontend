261ee2c02eccb1932c075cd0beb46b6cf3da9bb7
rafsan-jany <rafsan.mbstu@gmail.com>
2024-09-12 14:23:29 +0600

    delete operation added

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import { listEmployees } from '../services/EmployeeService'
+import { deleteEmployee, listEmployees } from '../services/EmployeeService'
 import { useNavigate } from 'react-router-dom'
 
 const ListEmployeeComponent = () => {
@@ -8,12 +8,16 @@ const ListEmployeeComponent = () => {
     const navigator = useNavigate();
 
     useEffect(() => {
+        getAllEmployees();
+    }, [])
+
+    function getAllEmployees(){
         listEmployees().then((response) => {
             setEmployees(response.data)
         }).catch(error => {
             console.log(error)
         })
-    }, [])
+    }
 
     function addNewEmployee() {
         navigator('/add-employee')
@@ -21,6 +25,16 @@ const ListEmployeeComponent = () => {
 
     function updateEmployee(id) {
         navigator(`/edit-employee/${id}`)
+    }
+
+    function removeEmployee(id){
+        console.log(id);
+        deleteEmployee(id).then((response) => {
+            getAllEmployees();
+            console.log(response.data)
+        }).catch(error => {
+            console.log(error)
+        })
     }
 
   return (
@@ -47,6 +61,9 @@ const ListEmployeeComponent = () => {
                             <td>{employee.email}</td>
                             <td>
                                 <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
+                                <button className='btn btn-danger' onClick={() => removeEmployee(employee.id)}
+                                    style={{marginLeft:'10px'}}
+                                    >Delete</button>
                             </td>
                         </tr>
                     )
